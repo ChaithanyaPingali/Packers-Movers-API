2c1ce8e22d41bf820d579f9caec5a36ec7abea9c
vamsi2711 <krishnavamsisomu@gmail.com>
2023-04-22 14:55:27 -0500

    modified code in SetUpServImpl file

--- src/main/java/com/movesmart/movesmartapi/service/ReadSetupServiceImpl.java
+++ src/main/java/com/movesmart/movesmartapi/service/ReadSetupServiceImpl.java
@@ -4,8 +4,8 @@ package com.movesmart.movesmartapi.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
